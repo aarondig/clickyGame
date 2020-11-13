f8cebe906936d99081046e00179312566afe5174
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-12 16:57:20 -0800

    Created scoring nav bar, added point system, and began to work on card shuffle.

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,10 +1,13 @@
-// import React from "react";
-// import "./style.css";
+import React from "react";
+import "./style.css";
 
-// function Header(props) {
-//   return <div className="nav">
-      
-//   </div>;
-// }
+function Banner(props) {
+  return <nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">
+                <h1 className="title">Memory Game</h1>
+                <h2 className="score">Score: {props.score} High Score: {props.highscore}</h2>
+            </span>
+        </nav>
+}
 
-// export default Header;
+export default Banner;
--- src/components/game.js
+++ src/components/game.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Wrapper from "./wrapper";
+import Banner from "./banner";
 import Cards from "./cards";
 import Shapes from "../shapes.json";
 
@@ -9,12 +10,8 @@ class Game extends Component {
     highscore: 0,
     data: Shapes,
     clicks: [],
-    endGame: false,
     alert: "Click an image to start."
   };
-
-
-
 
   handleClick = (event) => {
     // Getting the value and name of the input which triggered the change
@@ -23,26 +20,51 @@ class Game extends Component {
     var ran = 0;
     for (var i = 0; i < this.state.clicks.length; i++) {
       if (id === this.state.clicks[i]) {
-        // this.setState({
-        //   alert: "You have already selected this image. Game Over",
-        // });
-        this.setState({ endGame: true });
-        console.log(this.state.endGame);
+        this.setState({
+          alert: "You have already selected this image. Game Over",
+        });
         ran = 1;
+        this.endGame();
       } 
     }
     if (ran === 0) {
       this.setState({ score: this.state.score + 1 });
       this.state.clicks.push(id);
+      console.log(this.state.data);
+      this.shuffle();
     } 
   };
 
+shuffle = function() {
+// this.state.data.map((props) => (
+//   {props.order} = math.random
+// ))
+//   for (i = 0; i < this.state.data.length; i++) {
+
+
   
+  
+// }
+this.state.data.sort(() => (Math.random() - 1));
+console.log(this.state.data);
+}
+
+
+  endGame = function() {
+
+    if (this.state.score > this.state.highscore){
+      this.setState({highscore: this.state.score})
+    }
+    this.setState({score: 0});
+    this.setState({clicks: []});
+    this.setState({alert: "Click an image to start."});
+  }
 
 
   render() {
     return (
       <Wrapper>
+        <Banner score={this.state.score} highscore={this.state.highscore}/>
         <h1 className="title">Shape Memory Game</h1>
         <Cards data={this.state.data} handleClick={this.handleClick} />
       </Wrapper>
